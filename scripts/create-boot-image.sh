dcf6be244d10638442548a7a0df68e858689a69f
Andrew F. Davis <afd@ti.com>
2018-05-14 14:27:03 -0500

    Initial commit
    Signed-off-by: Andrew F. Davis <afd@ti.com>

---
+++ scripts/create-boot-image.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+function fn_display_usage {
+	echo "Error: $1"
+	echo ""
+	echo "This script is used to create a bootable image for the default platform."
+	echo ""
+	echo "Usage: create-boot-image.sh <unused> <input-file-name> <output-file-name>"
+	echo ""
+	exit 1
+}
+
+# validate input parameter
+if [ $# -lt 3 ]
+then
+	fn_display_usage "missing parameter"
+fi
+
+echo ""
+echo "Warning! This is a dummy signing tool, it is meant for build testing only."
+echo "This will NOT produce an image that is bootable on HS devices!"
+echo ""
+
+cp $2 $3
